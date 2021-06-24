96155166ee814d53b1f081e470b32708eaa99431
Vikos <vasserman.viktoria@yandex.ru>
2021-06-24 11:47:15 +0700

    initial

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const express=require('express')
+const config=require('config')
+const mongoose=require('mongoose')
 const app=express()
-app.listen(5000, () => console.log('App has been started...'))
+const PORT=config.get('port') || 5000
+async function  start(){
+    try{
+       await mongoose.connect(config.get('mongoURI'),{
+
+       })
+    }
+    catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+
+}
+start()
+app.listen(PORT, () => console.log('App has been started on port ${PORT}'))
